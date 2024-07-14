e65c30be3fafd03d6f3d9f6ae1013a3385b412f6
Huy Pham <huypham0297@gmail.com>
2024-07-14 20:51:31 +0700

    feat: add ex4 for lesson 4

--- src/main/java/vn/edu/cybersoft/pkg/utils.java
+++ src/main/java/vn/edu/cybersoft/pkg/utils.java
@@ -15,6 +15,16 @@ public class utils {
         return temp;
     }
 
+    public static double furthest(double[] list){
+        double temp = 0;
+        for(int i = 0; i < list.length; i++){
+            if (temp < list[i]){
+                temp =  list[i];
+            }
+        }
+        return temp;
+    }
+
     public static boolean duplicated(int value, int expectedValue){
         return value == expectedValue;
     }
